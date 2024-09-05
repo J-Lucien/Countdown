d55fbbb0288196c9a390b110cdb44ffd50eadfd0
J-Lucien <jtolojanahary5@gmail.com>
2024-09-05 20:47:06 +0300

    Remove reset button

--- src/components/Countdown/Datepicker.js
+++ src/components/Countdown/Datepicker.js
@@ -13,40 +13,53 @@ const StyledInput = styled.input`
 export default class Datepicker extends Component{
 
     state = {
-        date:'2018-12-12'
+        date:'',
+        valid:true,
+        dirty:false
     }
 
-    handleDateChange = (e) =>{
+    handleDateChange = ({target:{value}}) =>{
+        const date = moment(value),
+            valid = date.isValid() && date.isAfter(moment())
         this.setState({
-            date: e.target.value
+            date: value,
+            valid,
+            dirty:true
         })
+
+        valid && this.props.onDateReset(date)
     }
 
-    handleDateSubmit =(e) =>{
-        e.preventDefault()
-        const date = moment(this.state.date);
-        date.isValid() && date.isAfter(moment()) && this.props.onDateReset(date)
-    }
+    // handleDateSubmit =(e) =>{
+    //     e.preventDefault()
+    //     const date = moment(this.state.date);
+    //     date.isValid() && date.isAfter(moment()) && this.props.onDateReset(date)
+    // }
 
     render(){
 
-        const {date} = this.state
+        let {date,valid,dirty} = this.state,
+            classes = 'input is-medium is-rounded has-background-grey-lighter has-text-black';
+
+        valid && dirty && (classes += ' is-success') 
+        !valid && dirty && (classes += ' is-danger') 
 
         return <form onSubmit={this.handleDateSubmit} >
             <div className="field  is-grouped is-grouped-centered" style={{marginBottom:40}}>
                 <p className="control">
                     <StyledInput 
-                    className="input is-medium is-rounded has-background-grey-lighter has-text-black" 
+                    className={classes} 
                     type="text" 
                     value={date}
                     onChange={this.handleDateChange}
                     placeholder="Type a date.." />
+                    {!valid && <i className="help is-danger">Please enter valid (and future) date</i>}
                 </p>
-                <p className="control">
+                {/* <p className="control">
                     <button className="button is-light is-outlined is-medium is-rounded">
                     Reset
                     </button>
-                </p>
+                </p> */}
             </div>
         </form>
     }
